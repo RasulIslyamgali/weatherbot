25fd93c715f8a75ac4484c4ed2c1a791650f1c67
RasulIslyamgali <79205006+RasulIslyamgali@users.noreply.github.com>
2021-02-28 15:01:59 +0300

    Update weatherbot.py

--- weatherbot.py
+++ weatherbot.py
@@ -16,9 +16,15 @@ bot = telebot.TeleBot('1636422665:AAFBrkQpE-jvMR4Mcz6DsVnY3tfVQS7zlfw')
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
+while True:
+	try:
         observation = mgr.weather_at_place(message.text)
         w = observation.weather
 	#bot.reply_to(message, message.text)
+	break
+	
+	except NotFoundError:
+		answer = 'Город не найден'
 
 
         temp = w.temperature('celsius')["temp"]
